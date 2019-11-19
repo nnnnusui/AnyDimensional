6a8d5afcf18ef1fb8be4f47d2a21879ec64b43af
nnnnusui <nnnnusui@gmail.com>
2019-11-20 01:40:39 +0900

    ➕ implicit Coordinates#[toVector(), fromVector()]
    ex)
      val coordinates = Coordinates(0, 1, 2, 3, 4)
      println(coordinates(2)) //say: 2

--- src/main/scala/com/github/nnnnusui/anydimensional/Coordinates.scala
+++ src/main/scala/com/github/nnnnusui/anydimensional/Coordinates.scala
@@ -2,6 +2,9 @@ package com.github.nnnnusui.anydimensional
 
 object Coordinates{
   def apply[A](values: A*)(implicit numeric: Numeric[A]): Coordinates[A] = Coordinates(values.toVector)
+
+  implicit def toVector[A](that: Coordinates[A])(implicit numeric: Numeric[A]): Vector[A] = that.values
+  implicit def fromVector[A](that: Vector[A])(implicit numeric: Numeric[A]): Coordinates[A] = Coordinates(that)
 }
 case class Coordinates[A](values: Vector[A])(implicit numeric: Numeric[A]){
   val dimension = values.size
--- src/test/scala/com.github.nnnnusui.anydimensional/CoordinatesTest.scala
+++ src/test/scala/com.github.nnnnusui.anydimensional/CoordinatesTest.scala
@@ -4,16 +4,18 @@ import org.scalatest.{Matchers, WordSpec}
 import org.scalatestplus.scalacheck.ScalaCheckDrivenPropertyChecks
 
 class CoordinatesTest extends WordSpec with Matchers with ScalaCheckDrivenPropertyChecks {
-//  "Coordinates(*)" should {
-//    "Int" in
-//      forAll { (vec: Vector[Int]) =>
-//        println(Coordinates(vec))
-//      }
-//    "Double" in
-//      forAll { (vec: Vector[Double]) =>
-//        println(Coordinates(vec))
-//      }
-//  }
-  val coord = Coordinates(1, 5)
-  println(coord)
+  "Coordinates(*)" should {
+    "Int" in
+      forAll { (vec: Vector[Int]) =>
+        val coordinates = Coordinates(vec)
+        println(coordinates)
+      }
+    "Double" in
+      forAll { (vec: Vector[Double]) =>
+        val coordinates = Coordinates(vec)
+        println(coordinates)
+      }
+  }
+  val coordinates = Coordinates(1, 5)
+  println(coordinates(1))
 }
