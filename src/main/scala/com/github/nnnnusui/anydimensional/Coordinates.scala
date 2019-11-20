10e2085d9346aaa0af427fc3b3071d9da4e9f748
nnnnusui <nnnnusui@gmail.com>
2019-11-20 23:02:59 +0900

    📝 case class Coordinates[A] extends Is[Vector[A]]
    before:
      case class Coordinates[A] extends IsVector[A]

--- src/main/scala/com/github/nnnnusui/anydimensional/Coordinates.scala
+++ src/main/scala/com/github/nnnnusui/anydimensional/Coordinates.scala
@@ -2,13 +2,15 @@ package com.github.nnnnusui.anydimensional
 
 object Coordinates{
   def apply[A](values: A*)(implicit numeric: Numeric[A]): Coordinates[A] = Coordinates(values.toVector)
-
-  implicit def toVector[A](that: Coordinates[A])(implicit numeric: Numeric[A]): Vector[A] = that.values
-  implicit def fromVector[A](that: Vector[A])(implicit numeric: Numeric[A]): Coordinates[A] = Coordinates(that)
 }
-case class Coordinates[A](values: Vector[A])(implicit numeric: Numeric[A]) extends IsVector[A]{
-  val dimension = values.size
+case class Coordinates[A](value: Vector[A])(implicit numeric: Numeric[A]) extends Is[Vector[A]]{
+  object Convert extends ConvertibleIs[Coordinates[A], Vector[A]] {
+    override def apply(value: Vector[A]): Coordinates[A] = Coordinates(value)
+  }
 
+  import Convert.{toValue, fromValue}
   object VectorIsNumeric extends VectorIsNumeric[A]
   def +(x: Coordinates[A]): Coordinates[A] = VectorIsNumeric.plus(this, x)
+
+  override def toString: String = s"${this.getClass.getSimpleName}(${value.mkString(", ")})"
 }
--- src/test/scala/com.github.nnnnusui.anydimensional/CoordinatesTest.scala
+++ src/test/scala/com.github.nnnnusui.anydimensional/CoordinatesTest.scala
@@ -4,21 +4,31 @@ import org.scalatest.{Matchers, WordSpec}
 import org.scalatestplus.scalacheck.ScalaCheckDrivenPropertyChecks
 
 class CoordinatesTest extends WordSpec with Matchers with ScalaCheckDrivenPropertyChecks {
-  "Coordinates(*)" should {
-    "Int" in
-      forAll { (vec: Vector[Int]) =>
-        val coordinates = Coordinates(vec)
-        println(coordinates)
-      }
-    "Double" in
-      forAll { (vec: Vector[Double]) =>
-        val coordinates = Coordinates(vec)
-        println(coordinates)
-      }
-  }
-  val coordinates = Coordinates(1, 5)
-  println(
-    Coordinates(1, 1)
-    + Coordinates(1, 1)
-  )
+
+  val coord1 = Coordinates(1, 2, 3)
+  val coord2 = Coordinates(4, 5, 6)
+  println(coord1 + coord2)
+
+
+
+
+
+
+//  "Coordinates(*)" should {
+//    "Int" in
+//      forAll { (vec: Vector[Int]) =>
+//        val coordinates = Coordinates(vec)
+//        println(coordinates)
+//      }
+//    "Double" in
+//      forAll { (vec: Vector[Double]) =>
+//        val coordinates = Coordinates(vec)
+//        println(coordinates)
+//      }
+//  }
+//  val coordinates = Coordinates(1, 5)
+//  println(
+//    Coordinates(1, 1)
+//    + Coordinates(1, 1)
+//  )
 }
