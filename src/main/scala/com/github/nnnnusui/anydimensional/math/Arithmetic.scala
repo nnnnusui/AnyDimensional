45b72d375878c867f6f7a903ae329cc03049d19b
nnnnusui <nnnnusui@gmail.com>
2019-11-22 18:25:08 +0900

    📝 rewrite math.Arithmetic

--- src/main/scala/com/github/nnnnusui/anydimensional/math/Arithmetic.scala
+++ src/main/scala/com/github/nnnnusui/anydimensional/math/Arithmetic.scala
@@ -1,28 +1,8 @@
 package com.github.nnnnusui.anydimensional.math
 
+import com.github.nnnnusui.anydimensional.math.calculate.{Div, Minus, Plus, Times}
+
 trait Arithmetic[T] extends Field[T]
-
-/* MEMO:
- * extends ではなく implicit であるべきな気もする。
- */
-/* 群 */trait Group[T] extends Addition[T] with Subtraction[T]
-/* 環 */trait Ring[T] extends Group[T] with Multiplication[T] with Negate[T]
-/* 体 */trait Field[T] extends Ring[T] with Division[T]
-
-trait Addition[T] {
-  // identity element = 1
-  def plus(x: T, y: T): T
-}
-trait Subtraction[T] {
-  def minus(x: T, y: T): T
-}
-trait Multiplication[T]{
-  def times(x: T, y: T): T
-}
-trait Division[T]{
-  def div(x: T, y: T): T
-}
-
-trait Negate[T]{
-  def negate(x: T): T
-}
+trait Field[T] extends Ring[T]  with Div[T]
+trait Ring[T]  extends Group[T] with Times[T]
+trait Group[T] extends Plus[T] with Minus[T]
