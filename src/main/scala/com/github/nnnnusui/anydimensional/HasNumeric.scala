d32ac4b8c14e3666b66d2ac011e23c1fd029352d
nnnnusui <nnnnusui@gmail.com>
2019-11-27 01:29:49 +0900

    ♻️ remove [HasNumeric, VectorIsNumeric]

--- src/main/scala/com/github/nnnnusui/anydimensional/HasNumeric.scala
+++
@@ -1,3 +0,0 @@
-package com.github.nnnnusui.anydimensional
-
-abstract class HasNumeric[A](implicit val numeric: Numeric[A])
--- src/test/scala/com/github/nnnnusui/anydimensional/math/IterableIsArithmeticTest.scala
+++ src/test/scala/com/github/nnnnusui/anydimensional/math/IterableIsArithmeticTest.scala
@@ -3,17 +3,6 @@ package com.github.nnnnusui.anydimensional.math
 import org.scalatest.FunSuite
 
 class IterableIsArithmeticTest extends FunSuite {
-
-//  implicit def isConvertable[A, B, I[X] <: Iterable[X]](from: I[A])(implicit f: A => B): Iterable[B] = from.map(it=> f(it))
-//
-//
-//  implicit class Convertable[Lhs[_] <: Iterable[_], A](lhs: Lhs[A]){
-//    def isConvertable[Rhs[X] >: Iterable[X], B](rhs: Rhs[B])(implicit f: B => A): Rhs[A] = lhs.map(it=> f(it))
-//  }
-//  println(1.isConvertable(2.0))
-//  implicit class ConvVec[A](lhs: Vector[A]){
-//    implicit def conv[B](rhs: Vector[B])(implicit f: B => A): Vector[A] = rhs.map(it=>f(it))
-//  }
   println(Vector(1) + Vector(1, 1))
   println(Seq(1, 1) + Vector(1))
   println(Seq(1, 1) + Seq(1, 1))
