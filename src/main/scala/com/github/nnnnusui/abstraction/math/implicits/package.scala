a866a6d27c25e74b1c11ac09bbe09f360e6b13a7
nnnnusui <nnnnusui@gmail.com>
2019-11-29 02:06:39 +0900

    📝➕ iterable's implicit def { Minus, Times, Div }

--- src/main/scala/com/github/nnnnusui/abstraction/math/implicits/package.scala
+++ src/main/scala/com/github/nnnnusui/abstraction/math/implicits/package.scala
@@ -11,6 +11,13 @@ package object implicits {
 
   implicit def iterableHasPlus[A, B[X] <: Iterable[X]](implicit has: Plus[A]): Plus[B[A]]
     = (x: B[A], y: B[A]) => x.zip(y).map(it => has.plus(it._1, it._2)).asInstanceOf[B[A]]
+  implicit def iterableHasMinus[A, B[X] <: Iterable[X]](implicit has: Minus[A]): Minus[B[A]]
+    = (x: B[A], y: B[A]) => x.zip(y).map(it => has.minus(it._1, it._2)).asInstanceOf[B[A]]
+  implicit def iterableHasTimes[A, B[X] <: Iterable[X]](implicit has: Times[A]): Times[B[A]]
+    = (x: B[A], y: B[A]) => x.zip(y).map(it => has.times(it._1, it._2)).asInstanceOf[B[A]]
+  implicit def iterableHasDiv[A, B[X] <: Iterable[X]](implicit has: Div[A]): Div[B[A]]
+    = (x: B[A], y: B[A]) => x.zip(y).map(it => has.div(it._1, it._2)).asInstanceOf[B[A]]
+
   implicit def iterableParameterImplicitConversion[A, B, C[X] <: Iterable[X]](before: C[A])(implicit converter: A => B): C[B]
     = before.map(it=> converter(it)).asInstanceOf[C[B]]
 
--- src/test/scala/com/github/nnnnusui/abstraction/math/implicits/packageTest.scala
+++ src/test/scala/com/github/nnnnusui/abstraction/math/implicits/packageTest.scala
@@ -1,6 +1,6 @@
 package com.github.nnnnusui.abstraction.math.implicits
 
-import com.github.nnnnusui.abstraction.math.calculation.Plus
+import com.github.nnnnusui.abstraction.math.calculation.{Minus, Plus, Times}
 import org.scalatest.FunSuite
 
 class packageTest extends FunSuite {
@@ -14,17 +14,30 @@ class packageTest extends FunSuite {
   implicit def iToJ(i: I): J = J(i.value)
   implicit def jToI(j: J): I = I(j.value.toInt)
 
-  /* iterable's parameter implicit conversion
-   * if (implicitly[I => J])
-   *   Iterable[I] => Iterable[J] is implicitly
-   */
-  val iSeq: Seq[I] = Seq(I(1), I(1), I(2))
-  println(iSeq + iSeq)
-  val jSeq: Seq[J] = Seq(J(1.2), J(2.4), J(3.2))
-  println(jSeq + jSeq)
-  println(iSeq + jSeq)
-  println(jSeq + iSeq)
+  { // iterable's implicit def
+    /* iterable's parameter implicit conversion
+     * if (implicitly[I => J])
+     *   Iterable[I] => Iterable[J] is implicitly
+     */
+    val iSeq: Seq[I] = Seq(I(1), I(1), I(2))
+    println(iSeq + iSeq)
+    val jSeq: Seq[J] = Seq(J(1.2), J(2.4), J(3.2))
+    println(jSeq + jSeq)
+    println(iSeq + jSeq)
+    println(jSeq + iSeq)
 
-  def intVec(v: Vector[J]): Vector[J] = v
-  intVec(Vector(I(1), I(2), I(3)))
+    def intVec(v: Vector[J]): Vector[J] = v
+    intVec(Vector(I(1), I(2), I(3)))
+  }
+  {
+    val iSeq = Seq(1, 1, 2)
+    val jSeq = Seq(1.2, 2.4, 3.2)
+//    val a = numericHasPlus(1)
+//    println(a.plus(1, 2))
+//    implicit def intHasPlus: Plus[Int] = (x: Int, y: Int)=> x + y
+
+    println(iSeq + iSeq)
+    println(iSeq * iSeq)
+    println(jSeq * jSeq)
+  }
 }
