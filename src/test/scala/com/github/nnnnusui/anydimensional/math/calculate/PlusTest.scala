940eed09d9c0c55e4e4508b3e1cea5cfb1777adf
nnnnusui <nnnnusui@gmail.com>
2019-11-28 17:23:47 +0900

    📝 implicit enrich operation sample

--- src/test/scala/com/github/nnnnusui/anydimensional/math/calculate/PlusTest.scala
+++ src/test/scala/com/github/nnnnusui/anydimensional/math/calculate/PlusTest.scala
@@ -2,9 +2,9 @@ package com.github.nnnnusui.anydimensional.math.calculate
 
 class PlusTest extends org.scalatest.FunSuite {
   case class I(value: Int)
-  implicit val aHasPlus: Plus[I] = (x: I, y: I) => I(x.value + y.value)
+  implicit val iHasPlus: Plus[I] = (x: I, y: I) => I(x.value + y.value)
 
-  class PlusOperator[A: Plus](lhs: A){
+  implicit class EnrichPlusOperator[A: Plus](lhs: A){
     def +(rhs: A): A = implicitly[Plus[A]].plus(lhs, rhs)
   }
 
