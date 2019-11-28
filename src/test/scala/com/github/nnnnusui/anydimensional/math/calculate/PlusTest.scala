25444c8b1152c012f021462dd713aa56bcaa5831
nnnnusui <nnnnusui@gmail.com>
2019-11-28 20:52:04 +0900

    📝 write example: [I <: Iterable, A: Plus] I[A] + I[A]

--- src/test/scala/com/github/nnnnusui/anydimensional/math/calculate/PlusTest.scala
+++ src/test/scala/com/github/nnnnusui/anydimensional/math/calculate/PlusTest.scala
@@ -3,20 +3,36 @@ package com.github.nnnnusui.anydimensional.math.calculate
 class PlusTest extends org.scalatest.FunSuite {
   case class I(value: Int)
   implicit val iHasPlus: Plus[I] = (x: I, y: I) => I(x.value + y.value)
-
   case class J(value: Double)
   implicit val jHasPlus: Plus[J] = (x: J, y: J) => J(x.value + y.value)
 
-  implicit class EnrichPlusOperator[Left](lhs: Left){
-    def +[Right: Plus](rhs: Right)(implicit lToR: Left => Right): Right = implicitly[Plus[Right]].plus(lhs, rhs)
+  implicit class EnrichPlusOperator[T](lhs: T){
+    def +[Right](rhs: Right)(implicit rToL: Right => T, has: Plus[T]): T = has.plus(lhs, rhs)
   }
 
-  println(I(1) + I(2))
-  println(J(2) + J(4))
+  implicit def iterableHasPlus[A, B[X] <: Iterable[X]](implicit has: Plus[A]): Plus[B[A]] = (x: B[A], y: B[A]) => x.zip(y).map(it => has.plus(it._1, it._2)).asInstanceOf[B[A]]
 
-  implicit def IToJ(i: I): J = J(i.value)
-  implicit def JToI(j: J): I = I(j.value.toInt)
+  /* Arithmetic iterable
+   * Iterable -> Seq -> List
+   * Iterable -> IndexedSeq -> Vector
+   *  allow: -T
+   *  deny : +T
+   */
+  val i = Iterable(I(1), I(1), I(2))
+  println(i.toIterable + i.toIterable)
+  println(i.toIterable + i.toSeq     )
+  println(i.toIterable + i.toList    )
+  println(i.toSeq      + i.toIterable) //error: NotImplicitly[Iterable => Seq]
+  println(i.toSeq      + i.toSeq     )
+  println(i.toSeq      + i.toList    )
+  println(i.toList     + i.toIterable) //error: NotImplicitly[Iterable => List]
+  println(i.toList     + i.toSeq     ) //error: NotImplicitly[Seq => List]
+  println(i.toList     + i.toList    )
+  println(i.toIterable + i.toIndexedSeq)
+  println(i.toIterable + i.toVector    )
+  println(i.toIndexedSeq + i.toIndexedSeq)
+  println(i.toIndexedSeq + i.toVector    )
+  println(i.toVector + i.toIndexedSeq) //error: NotImplicitly[IndexedSeq => Vector]
+  println(i.toVector + i.toVector    )
 
-  println(I(2) + J(4))
-  println(J(2) + I(4))
 }
