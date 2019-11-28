81f1836c1cff84e3c01680c508ea85585036d553
nnnnusui <nnnnusui@gmail.com>
2019-11-28 17:16:52 +0900

    📝 OperatorSampleCode in PlusTest.scala

--- src/test/scala/com/github/nnnnusui/anydimensional/math/calculate/PlusTest.scala
+++ src/test/scala/com/github/nnnnusui/anydimensional/math/calculate/PlusTest.scala
@@ -3,5 +3,11 @@ package com.github.nnnnusui.anydimensional.math.calculate
 class PlusTest extends org.scalatest.FunSuite {
   case class I(value: Int)
   implicit val aHasPlus: Plus[I] = (x: I, y: I) => I(x.value + y.value)
-  println(aHasPlus.plus(I(1), I(2)))
+
+  class PlusOperator[A: Plus](lhs: A){
+    def +(rhs: A): A = implicitly[Plus[A]].plus(lhs, rhs)
+  }
+
+  val plop = new PlusOperator[I](I(1))
+  println(plop + I(2))
 }
