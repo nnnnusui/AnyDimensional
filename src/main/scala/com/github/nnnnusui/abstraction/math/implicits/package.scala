22048c9cf3f06082b44cc82aa9345261e05f5a8c
nnnnusui <nnnnusui@gmail.com>
2019-11-29 02:06:11 +0900

    📝➕ implicit def { integralIsField, fractionalIsField }

--- src/main/scala/com/github/nnnnusui/abstraction/math/implicits/package.scala
+++ src/main/scala/com/github/nnnnusui/abstraction/math/implicits/package.scala
@@ -13,4 +13,11 @@ package object implicits {
     = (x: B[A], y: B[A]) => x.zip(y).map(it => has.plus(it._1, it._2)).asInstanceOf[B[A]]
   implicit def iterableParameterImplicitConversion[A, B, C[X] <: Iterable[X]](before: C[A])(implicit converter: A => B): C[B]
     = before.map(it=> converter(it)).asInstanceOf[C[B]]
+
+  implicit def integralIsField[T](implicit integral: Integral[T]): IntegralIsField[T] = new IntegralIsField[T] {
+    override implicit val has: Integral[T] = integral
+  }
+  implicit def fractionalIsField[T](implicit fractional: Fractional[T]): FractionalIsField[T] = new FractionalIsField[T] {
+    override implicit val has: Fractional[T] = fractional
+  }
 }
