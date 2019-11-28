563b29a0c387c622e932879bfad4429d47184ae7
nnnnusui <nnnnusui@gmail.com>
2019-11-28 17:24:53 +0900

    📝 add J to sample

--- src/test/scala/com/github/nnnnusui/anydimensional/math/calculate/PlusTest.scala
+++ src/test/scala/com/github/nnnnusui/anydimensional/math/calculate/PlusTest.scala
@@ -4,10 +4,17 @@ class PlusTest extends org.scalatest.FunSuite {
   case class I(value: Int)
   implicit val iHasPlus: Plus[I] = (x: I, y: I) => I(x.value + y.value)
 
+  case class J(value: Int)
+  implicit val jHasPlus: Plus[J] = (x: J, y: J) => J(x.value + y.value)
+
   implicit class EnrichPlusOperator[A: Plus](lhs: A){
     def +(rhs: A): A = implicitly[Plus[A]].plus(lhs, rhs)
   }
 
-  val plop = new PlusOperator[I](I(1))
-  println(plop + I(2))
+  println(I(1) + I(2))
+  println(J(2) + J(4))
+
+  implicit def JToI(j: J): I = I(j.value)
+  println(I(2) + JToI(J(4)))
+  println(I(2) + J(4)) // error
 }
