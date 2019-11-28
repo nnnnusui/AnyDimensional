658cf3ea6ca66dc664f99137fdb300945823f2dc
nnnnusui <nnnnusui@gmail.com>
2019-11-28 20:57:59 +0900

    ➕📄 math.calculate.{(Plus, )Minus, Div, Times}

---
+++ src/main/scala/com/github/nnnnusui/anydimensional/math/calculate/Minus.scala
@@ -0,0 +1,5 @@
+package com.github.nnnnusui.anydimensional.math.calculate
+
+trait Minus[T] {
+  def minus(x: T, y: T): T
+}
---
+++ src/main/scala/com/github/nnnnusui/anydimensional/math/calculate/Times.scala
@@ -0,0 +1,5 @@
+package com.github.nnnnusui.anydimensional.math.calculate
+
+trait Times[T] {
+  def times(x: T, y: T): T
+}
