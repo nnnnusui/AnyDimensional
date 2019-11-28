75f8f8b6192defff28f4a52dadab0cd028c7fa41
nnnnusui <nnnnusui@gmail.com>
2019-11-28 21:41:48 +0900

    ♻️ example

--- src/test/scala/com/github/nnnnusui/anydimensional/math/calculate/PlusTest.scala
+++ src/test/scala/com/github/nnnnusui/anydimensional/math/calculate/PlusTest.scala
@@ -14,9 +14,9 @@ class PlusTest extends org.scalatest.FunSuite {
   }
 
   implicit def iterableHasPlus[A, B[X] <: Iterable[X]](implicit has: Plus[A]): Plus[B[A]]
-    = (x: B[A], y: B[A]) => x.zip(y).map(it => has.plus(it._1, it._2)).asInstanceOf[B[A]] /*<-不服*/
+    = (x: B[A], y: B[A]) => x.zip(y).map(it => has.plus(it._1, it._2)).asInstanceOf[B[A]]
   implicit def iterableParameterImplicitConversion[A, B, C[X] <: Iterable[X]](before: C[A])(implicit converter: A => B): C[B]
-    = before.map(it=> converter(it)).asInstanceOf[C[B]] /*<-不服*/
+    = before.map(it=> converter(it)).asInstanceOf[C[B]]
 
   /* iterable's parameter implicit conversion
    * if (implicitly[I => J])
